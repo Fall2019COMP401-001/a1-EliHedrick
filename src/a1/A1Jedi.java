af88830b66c75bf572df3c17c382fd3c1d1f5d21
Eli <Eli@LAPTOP-J514KVQG.wireless-1x.unc.edu>
2019-08-29 22:13:59 -0400

    First attempt at Jedi level

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,48 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int itemsInStore = scan.nextInt();
+		String[] items = new String[itemsInStore]; // holds names of all items
+		int[] customersBought = new int[itemsInStore]; // holds how many customers bought each item
+		int[] amountBought = new int[itemsInStore]; // holds amount of each item bought
+		for(int x = 0;x < itemsInStore;x++)
+		{
+			items[x] = scan.next(); // add item name to array
+			scan.next(); //skips price, not needed for jedi
+		}
 		
+		int customerAmount= scan.nextInt();
+		for(int x = 0; x < customerAmount; x++)
+		{
+			scan.next(); // skips first and last name
+			scan.next();
+			int itemsBought = scan.nextInt(); // gets how many items each customer bought
+			
+			for(int y = 0; y < itemsBought; y++) // checks each item
+			{
+				int dupeBought = scan.nextInt(); // how many of each item is bought
+				String itemName = scan.next();
+				int indexOfItem = -1;
+				
+				for(int z = 0; z < items.length; z++) // find index of item in array
+				{
+					if(items[z].equals(itemName))
+					{
+						indexOfItem = z;
+						customersBought[indexOfItem]++;
+						amountBought[indexOfItem]+= dupeBought;
+						break;
+					}
+				}
+			}
+		}
+		for(int x = 0; x < itemsInStore; x++)
+			if(customersBought[x] == 0)
+			{
+				System.out.println("No customers bought " + items[x]);
+			}
+			else
+			System.out.println(customersBought[x] + " customers bought "
+					+ amountBought[x] + " " + items[x]);
 	}
 }
